e7f79fd38a73b4a226248aea7a044c544adb454c
JIn <127292202+JInsunnya@users.noreply.github.com>
2025-01-22 23:07:14 +0900

    Title: 몫 출력하기, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250122/몫 출력하기/print-share.py
@@ -0,0 +1,12 @@
+cnt = 0
+
+while True:
+    i = int(input())
+    if i % 2 != 0:
+        pass
+    else:
+        print(i // 2)
+        cnt += 1
+        if cnt == 3:
+            break
+        
