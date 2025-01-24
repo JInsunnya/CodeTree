d1dc95ec1fcfd0e8d03db9caeaf1e9f497fd1dfd
JIn <127292202+JInsunnya@users.noreply.github.com>
2025-01-24 07:11:51 +0530

    Title: ab 사이에 있는 c, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250124/ab 사이에 있는 c/c-between-a-and-b.py
+++ 250124/ab 사이에 있는 c/c-between-a-and-b.py
@@ -2,7 +2,7 @@ arr = input().split()
 a,b,c = int(arr[0]), int(arr[1]), int(arr[2])
 
 satisfied = False
-for i in range(a, b):
+for i in range(a, b+1):
     if i % c == 0:
         satisfied = True
 
