306631dec7b8280fabdfb2990920cb54aa1f4fdb
JIn <127292202+JInsunnya@users.noreply.github.com>
2025-01-27 19:08:30 +0530

    Title: 소수 판별기, Time: 40ms, Memory: 16MB, Status: Passed - CodeTree

--- 250127/소수 판별기/prime-number-discriminator.py
+++ 250127/소수 판별기/prime-number-discriminator.py
@@ -1,11 +1,19 @@
-n=int(input())
+n = int(input())
 
-for i in range(2,n):
-    if n%i!=0:
-        print("P")
-    else:
-        print("C")
-    break
+satis = True
+
+i = 2
+while i < n :
+    if n % i == 0:
+        satis = False
+        break
+    i += 1
+
+if satis == True:
+    print("P")
+else:
+    print("C")
+
 
 
 # n = int(input())
