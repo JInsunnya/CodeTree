2697e16f386cffc64a7c22519997221aa4c09e48
JIn <127292202+JInsunnya@users.noreply.github.com>
2025-01-29 20:12:35 +0530

    Title: 직사각형 별표 출력하기, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250129/직사각형 별표 출력하기/print-stars-in-rectangle.py
+++ 250129/직사각형 별표 출력하기/print-stars-in-rectangle.py
@@ -1,14 +1,8 @@
-n, m = map(int, input().split())
+arr = input().split()
+n = int(arr[0])
+m = int(arr[1])
 
 for _ in range(n):
-    print("*" * m)  # 한 줄에 m개의 별을 출력하고 자동으로 줄 바꿈
-
-
-# arr = input().split()
-# n = int(arr[0])
-# m = int(arr[1])
-
-# for _ in range(n):
-#     for _ in range(m):
-#         print("*", end="")
-#     print("")
+    for _ in range(m):
+        print("*", end=" ")
+    print("")
