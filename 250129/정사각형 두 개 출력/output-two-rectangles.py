63804a1acf69802744a0cb542a88793a5dc39a64
JIn <127292202+JInsunnya@users.noreply.github.com>
2025-01-29 20:17:55 +0530

    Title: 정사각형 두 개 출력, Time: 39ms, Memory: 16MB, Status: Passed - CodeTree

--- 250129/정사각형 두 개 출력/output-two-rectangles.py
+++ 250129/정사각형 두 개 출력/output-two-rectangles.py
@@ -1,14 +1,6 @@
 n = int(input())
 
-for _ in range(2):  # 두 개의 블록을 출력
+for _ in range(2):  
     for _ in range(n):
-        print("*" * n)  # n개의 '*' 출력
-    print()  # 블록 사이에 한 줄 띄우기
-
-# n = int(input())
-
-# for _ in range(2):
-#     for _ in range(n):
-#         for _ in range(n):
-#             print("*", end="")
-#         print("")
+        print("*" * n)  
+    print()  
