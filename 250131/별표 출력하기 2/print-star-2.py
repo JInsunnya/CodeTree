570402dc018e1a3bc6f2fdf1aed5061417943280
JIn <127292202+JInsunnya@users.noreply.github.com>
2025-01-31 06:46:29 +0530

    Title: 별표 출력하기 2, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250131/별표 출력하기 2/print-star-2.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+while n >= 1:
+    for j in range(n):
+        print("*", end = " ")
+    print()
+    n -= 1
+    
+
+
+# n = int(input())
+
+# for i in range(n):
+#     for j in range(i-1):
+#         print("*", end=" ")
+#     print()
